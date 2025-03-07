76db612634716e6383d5b683a8dd7340eae3db7d
Cristian <krissomc@hotmail.com>
2025-03-06 23:33:28 -0500

    archivo incompleto

--- server.js
+++ server.js
@@ -6,6 +6,14 @@ const app = express();
 // Servir archivos estáticos desde el directorio 'dist'
 app.use(express.static(path.join(__dirname, 'dist')));
 
+// Middleware para asegurar que los archivos .js se sirvan con el tipo MIME adecuado
+app.use((req, res, next) => {
+  if (req.url.endsWith('.js')) {
+    res.setHeader('Content-Type', 'application/javascript');
+  }
+  next();
+});
+
 // Manejar todas las demás rutas y servir 'index.html'
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'dist', 'index.html'));
